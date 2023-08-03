844c2a4dd4f6403682be58bced5c81d6b6d79774
Justin Abuyuan <abuyuanjustin@gmail.com>
2023-08-03 00:16:03 -0400

    added text

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ export default function App() {
   return (
     <>
       <h1>This is an image.</h1>
-      <img src="src/assets/images/placeholder-image.png"></img>
+      <img src="/images/placeholder-image.png"></img>
+      <h2>This is a subtitle.</h2>
+      <p>yeah.</p>
     </>
   )
 }
